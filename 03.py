198d0d5ee64c1a90e6709d308dd3818ed61d045e
haretaro <frontend.1000@gmail.com>
2017-12-22 11:00:45 +0900

    stripの使い方間違ってたので修正

--- 03.py
+++ 03.py
@@ -1,3 +1,3 @@
 s = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
-ans = [len(w.strip(',').strip('.')) for w in s.split(' ')]
+ans = [len(w.strip(',.')) for w in s.split(' ')]
 print(ans)
