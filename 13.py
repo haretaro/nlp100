6df06d2dd4b3b47882d6ee46a937cb6544f01a2f
haretaro <frontend.1000@gmail.com>
2018-01-05 11:40:54 +0900

    簡潔な書き方に直した

--- 13.py
+++ 13.py
@@ -1,4 +1,4 @@
 with open('col1.txt') as f1, open('col2.txt') as f2, open('merged.txt', 'w') as out:
-    for c1, c2 in zip(f1.readlines(), f2.readlines()):
+    for c1, c2 in zip(f1, f2):
         c1 = c1.rstrip('\n')
         out.write('{}\t{}'.format(c1, c2))
